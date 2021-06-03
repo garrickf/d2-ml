435121d5f9b51acc94c9e7fde2c1e0ba7920b736
Garrick Fernandez <20538779+garrickf@users.noreply.github.com>
2021-06-02 23:38:48 -0700

    update environment.yml file, data collection

--- src/data_collection/data_collection.py
+++ src/data_collection/data_collection.py
@@ -19,8 +19,11 @@ logging.getLogger("urllib3.connectionpool").setLevel(logging.WARNING)
 
 # Constants
 STARTING_ACTIVITY_ID = 8400554258
-ENDING_ACTIVITY_ID = STARTING_ACTIVITY_ID + int(100)
-# ENDING_ACTIVITY_ID = STARTING_ACTIVITY_ID + int(1e6)
+# ENDING_ACTIVITY_ID = STARTING_ACTIVITY_ID + int(100)
+# ENDING_ACTIVITY_ID = STARTING_ACTIVITY_ID + int(1000)  # 1K
+# ENDING_ACTIVITY_ID = STARTING_ACTIVITY_ID + int(10000)  # 10K
+ENDING_ACTIVITY_ID = STARTING_ACTIVITY_ID + int(100000)  # 100K
+# ENDING_ACTIVITY_ID = STARTING_ACTIVITY_ID + int(1e6)  # 1M
 
 # TODO pull out into common utils
 def get_headers():
@@ -56,7 +59,7 @@ GET_POST_GAME_CARNAGE_REPORT = "Destiny2/Stats/PostGameCarnageReport/{activityId
 # print(json.dumps(r.json(), indent=4))  # Example of prettyprint
 
 
-def get_activity_name(activity):
+def get_director_activity_name(activity):
     # Uses the directorActivityHash of the
     # HistoricalStats.DestinyHistoricalStatsActivity (i.e., the entries of the
     # activity history)
@@ -89,41 +92,100 @@ def get_activity_values(activity):
 
 
 def get_value_kills(values):
-    return values["kills"]["basic"]["value"]
+    if "kills" in values:
+        return values["kills"]["basic"]["value"]
+    else:
+        return None
 
 
 def get_value_deaths(values):
-    return values["deaths"]["basic"]["value"]
+    if "deaths" in values:
+        return values["deaths"]["basic"]["value"]
+    else:
+        return None
 
 
 def get_value_assists(values):
     # NOTE: takes in values object from activity summary
-    return values["assists"]["basic"]["value"]
+    if "assists" in values:
+        return values["assists"]["basic"]["value"]
+    else:
+        return None
 
 
 def get_value_score(values):
-    return values["score"]["basic"]["value"]
+    if "score" in values:
+        return values["score"]["basic"]["value"]
+    else:
+        return None
 
 
 def get_value_completed(values):
     # 1 if completed, NOTE haven't seen not completed but assuming it's 0
-    return values["completed"]["basic"]["value"]
+    if "completed" in values:
+        return values["completed"]["basic"]["value"]
+    else:
+        return None
 
 
 def get_value_opponents_defeated(values):
     # TODO what is the difference between this and kills?
-    return values["opponentsDefeated"]["basic"]["value"]
+    if "opponentsDefeated" in values:
+        return values["opponentsDefeated"]["basic"]["value"]
+    else:
+        return None
+
+
+def get_value_combat_efficiency(values):
+    if "efficiency" in values:
+        return values["efficiency"]["basic"]["value"]
+    else:
+        return None
+
+
+def get_value_kdr(values):
+    if "killsDeathsRatio" in values:
+        return values["killsDeathsRatio"]["basic"]["value"]
+    else:
+        return None
+
+
+def get_value_kda(values):
+    if "killsDeathsAssists" in values:
+        return values["killsDeathsAssists"]["basic"]["value"]
+    else:
+        return None
 
 
 def get_value_activity_duration(values, in_units_of="seconds"):
     # Returns activity duration in seconds
     # TODO allow translation to other units if need be
-    return values["activityDurationSeconds"]["basic"]["value"]
+    if "activityDurationSeconds" in values:
+        return values["activityDurationSeconds"]["basic"]["value"]
+    else:
+        return None
+
+
+def get_value_team(values):
+    if "team" in values:
+        return values["team"]["basic"]["value"]
+    else:
+        return None
 
 
 def get_value_standing(values):
     # The API marks 1 as defeat, 0 as victory; we invert that
-    return 1 - values["standing"]["basic"]["value"]
+    if "standing" in values:
+        return 1 - values["standing"]["basic"]["value"]
+    else: 
+        return None
+
+
+def get_value_team_score(values):
+    if "teamScore" in values:
+        return values["teamScore"]["basic"]["value"]
+    else:
+        return None
 
 
 def get_activity_history(character_data, params=None):
@@ -138,7 +200,7 @@ def get_activity_history(character_data, params=None):
     for activity in activities:
         period = get_activity_period(activity)
         details = activity["activityDetails"]
-        activity_name = get_activity_name(activity)
+        activity_name = get_director_activity_name(activity)
         values = get_activity_values(activity)
         id = get_activity_instance_id(activity)
 
@@ -158,24 +220,48 @@ def get_activity_history(character_data, params=None):
 
 
 def scrape_pgcr(instance_id):
+    """Given an instance id, make a request to Platform API, scrape data from
+    the JSON response, and assembler and return a dict mapping column names to
+    values.
+
+    Args:
+      instance_id (int): The ID of the activity instance. Activities are 
+        numbered in roughly increasing order and uniquely identify an activity.
+    """
     path = urljoin(
         API_URL, GET_POST_GAME_CARNAGE_REPORT.format(**{"activityId": instance_id})
     )
-    r = requests.get(path, headers=HEADERS)
+    try:
+        r = requests.get(path, headers=HEADERS, timeout=1)
+    except requests.ConnectionError:
+        # A connection error occurred. Probably some network issue.
+        return None
+    except requests.Timeout:
+        # A timeout error occurred
+        return None
 
     try:
         pgcr = r.json()["Response"]
     except json.JSONDecodeError:
+        # This can mean we've gone over our request rate limit, or something else
         print(f"Error: got status {r.status_code}")
         # print(f"Error {r['ErrorStatus']} ({r['ErrorCode']}): {r['Message']}")
         # print(f"Throttle seconds {r['ThrottleSeconds']}")
         # raise RuntimeError
 
         # Just return silently
-        return
+        return None
 
     period = get_activity_period(pgcr)
-    director_activity_name = get_activity_name(pgcr)
+    director_activity_name = get_director_activity_name(pgcr)
+
+    # TODO: different games have different values; our implementation should be robust to this
+    # if director_activity_name != "Gambit":
+    #     return {
+    #         "instance_id": instance_id,
+    #         "period": period,
+    #         "director_activity_name": director_activity_name,
+    #     }
 
     entries = pgcr["entries"]
     # print(f"Activity {instance_id} has {len(entries)} entries...")
@@ -184,7 +270,28 @@ def scrape_pgcr(instance_id):
     for i, entry in enumerate(entries):
         # TODO handle extended and other props
         characterId = entry["characterId"]
-        player_entries[f"player{i + 1}_id"] = characterId
+        player_entries[f"player{i + 1}_char_id"] = characterId
+
+        # Values (kills, assists, etc.)
+        if "values" not in entry: 
+            continue
+        
+        values = entry["values"]
+        player_entries[f"player{i + 1}_kills"] = get_value_kills(values)
+        player_entries[f"player{i + 1}_deaths"] = get_value_deaths(values)
+        player_entries[f"player{i + 1}_assists"] = get_value_assists(values)
+        player_entries[f"player{i + 1}_kdr"] = get_value_kdr(values)
+        player_entries[f"player{i + 1}_kda"] = get_value_kda(values)
+        player_entries[f"player{i + 1}_efficiency"] = get_value_combat_efficiency(
+            values
+        )
+        player_entries[f"player{i + 1}_score"] = get_value_score(values)
+
+        player_entries[f"player{i + 1}_standing"] = get_value_standing(values)
+        player_entries[f"player{i + 1}_team_score"] = get_value_team_score(values)
+        player_entries[
+            f"player{i + 1}_activity_duration"
+        ] = get_value_activity_duration(values)
 
     # Return dict
     return {
@@ -200,7 +307,8 @@ def scrape_pgcrs(filter=None):
 
     for instance_id in range(STARTING_ACTIVITY_ID, ENDING_ACTIVITY_ID):
         entry = scrape_pgcr(instance_id)
-        data.append(entry)
+        if filter is None or entry["director_activity_name"] == filter:
+            data.append(entry)
 
     df = pd.DataFrame(data)
     df.to_csv("test.csv")
@@ -216,11 +324,14 @@ def scrape_pgcrs_multithreaded(filter=None):
         def make_func(id):
             def func():
                 entry = scrape_pgcr(id)
-                data.append(entry)
+                if entry == None:
+                    return  # Error happened/malformed entry
+                if filter is None or entry["director_activity_name"] == filter:
+                    data.append(entry)
 
             return func
 
-        # Needed to create a new scope
+        # Needed to create a new scope around the current value of instance_id
         t.schedule(make_func(instance_id))
 
     t.shutdown()
@@ -246,6 +357,8 @@ if __name__ == "__main__":
     start_perf_ctr = time.perf_counter()
 
     # scrape_pgcrs()
+    # scrape_pgcrs_multithreaded(filter="Gambit")
+    # scrape_pgcrs_multithreaded(filter="Control")
     scrape_pgcrs_multithreaded()
 
     time_elapsed = time.time() - start_time
